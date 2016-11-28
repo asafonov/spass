b5eef920ae8ddb66f39a365137bca7789f374b50
asafonov <me@asafonov.org>
2016-11-28 14:54:59 +0000

    some additional info about the user

--- spass/__main__.py
+++ spass/__main__.py
@@ -3,6 +3,7 @@ import spass.device
 
 def main():
     print(spass.device.get_device_id())
+    print(spass.device.get_username())
 
 if __name__ == "__main__":
     main()
--- spass/device.py
+++ spass/device.py
@@ -1,4 +1,10 @@
-import uuid
+import uuid, os, pwd
 
 def get_device_id():
     return uuid.getnode()
+
+def get_username():
+    return pwd.getpwuid(os.getuid()).pw_name
+
+def get_userid():
+    return os.getuid()
