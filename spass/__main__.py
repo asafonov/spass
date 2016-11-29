64e63d541f545bf0ee65da1d9bf81a8855d6d683
asafonov <me@asafonov.org>
2016-11-29 13:02:43 +0000

    setting existing passwords to spass

--- spass/__main__.py
+++ spass/__main__.py
@@ -2,20 +2,20 @@ import os, sys, getopt, importlib
 
 def main():
     argv = sys.argv[1:]
-    opts, args = getopt.getopt(argv, "", ["password=", "length=", "account=", "update", "generate", "get"])
+    opts, args = getopt.getopt(argv, "", ["password=", "length=", "account=", "update", "generate", "get", "set"])
     func = ''
     func_argv = {}
-    module = ''
+    module = 'spass.spass'
     for o, a in opts:
         if o == '--generate':
             func = 'generate'
             module = 'spass.password'
         if o == '--update':
             func = 'update'
-            module = 'spass.spass'
         if o == '--get':
             func = 'get'
-            module = 'spass.spass'
+        if o == '--set':
+            func = 'set'
         if o == '--password':
             func_argv['password'] = a
         if o == '--length':
--- spass/spass.py
+++ spass/spass.py
@@ -1,29 +1,36 @@
 import spass.storage, spass.password, spass.crypt
 
-def update(a):
-    if 'account' not in a:
-        raise ValueError("please specify account for update")
+def check_params(params, required):
+    for i in range(len(required)):
+        if required[i] not in params:
+            raise ValueError("Please specify " + required[i])
 
+def load_data():
     try:
         data = spass.storage.load()
     except:
         data = {}
+    return data
 
+def update(a):
+    check_params(a, ['account'])
+    data = load_data()
     password = spass.password.generate(a)
     data[a['account']] = spass.crypt.encrypt(password)
     spass.storage.save(data)
     return password
 
 def get(a):
-    if 'account' not in a:
-        raise ValueError("please specify account for get")
-
-    try:
-        data = spass.storage.load()
-    except:
-        data = {}
-
+    check_params(a, ['account'])
+    data = load_data()
     if a['account'] not in data:
         raise ValueError("account doesn't exist")
-
     return spass.crypt.encrypt(data[a['account']])
+
+def set(a):
+    check_params(a, ['account', 'password'])
+    data = load_data()
+    data[a['account']] = spass.crypt.encrypt(a['password'])
+    spass.storage.save(data)
+    return a['password']
+    
