048b5143196f7c5b03497771118a54d1c0793a07
asafonov <me@asafonov.org>
2017-01-13 15:55:26 +0000

    sample http daemon for spass

--- spass/__main__.py
+++ spass/__main__.py
@@ -34,5 +34,28 @@ def main():
             func_argv['file'] = a
     print(getattr(importlib.import_module(module), func)(func_argv))
 
+def daemon():
+    import socket
+
+    HOST, PORT = '', 9092
+
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind((HOST, PORT))
+    listen_socket.listen(1)
+    print('Serving HTTP on port ' + str(PORT) + ' ...')
+    while True:
+        client_connection, client_address = listen_socket.accept()
+        request = client_connection.recv(1024)
+        print(request)
+
+        http_response = """\
+HTTP/1.1 200 OK
+
+Hello world
+"""
+        client_connection.sendall(http_response.encode("utf-8"))
+        client_connection.close()
+
 if __name__ == "__main__":
     main()
