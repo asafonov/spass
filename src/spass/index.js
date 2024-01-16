ceb7e8bdef0d98b4410ff1ca0764bb302e12433f
asafonov <me@asafonov.org>
2024-01-16 16:47:41 +0300

    add headers

--- src/spass/index.js
+++ src/spass/index.js
@@ -1,8 +1,21 @@
 const http = require('http')
 
+const doOptions = res => {
+  res.writeHead(200, {
+    'Content-Type': 'text/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*'
+  })
+}
+
 const init = () => {
   http.createServer((req, res) => {
-    console.log(req)
+    console.log(req.method)
+    if (req.method === 'OPTIONS') {
+      doOptions(res)
+    }
+
+    res.end()
   }).listen(9092)
 }
 
